7dfacc74772f07fac8088cf49dde914ccd2c0e8a
Sukhada Sankpal <sukhadas@cybage.com>
2016-05-18 16:12:54 +0530

    ansible latest 2.0.2.0

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install -y --force-yes software-properties-common
-sudo apt-add-repository --force-yes ppa:ansible/ansible
+sudo apt-add-repository --force-yes ppa:ansible/ansible-1.9
 sudo apt-get -y update
 sudo apt-get -y --force-yes install ansible
