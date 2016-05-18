4bebe13a8b6087f757931295a1dd745ae57b6cb6
Sukhada Sankpal <sukhadas@cybage.com>
2016-05-18 15:58:50 +0530

    Installation file added

---
+++ install_ansible.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install -y --force-yes software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y --force-yes install ansible
